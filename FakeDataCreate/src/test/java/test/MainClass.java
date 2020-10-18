4e37f0f1db3dacb0e0428e8a6f9bb1fefcd2eb25
GuruKrishna <GuruKrishna@GuruKrishna>
2020-10-18 10:32:26 +0530

    Main Function

--- FakeDataCreate/src/test/java/test/MainClass.java
+++ FakeDataCreate/src/test/java/test/MainClass.java
@@ -2,16 +2,26 @@ package test;
 
 
 
+import java.lang.reflect.Field;
+
+import pojoClases.Contract;
+import pojoClases.datacreation;
 import ReUsable.ReusableFactory;
+import ReUsable.Utils;
 
 
 public class MainClass {
 
 	public static void main(String[] args) {
 		
-		
+		Utils.archiveAvailableFiles();
 		ReusableFactory ref =  new ReusableFactory(); 
 		ref.createData(Constants.totalData);
+		//datacreation d =  new datacreation();
+		//d.casteGeneration();
+		
+		
+		//Utils.generateFolderwithName();
 		
 		
 	}
