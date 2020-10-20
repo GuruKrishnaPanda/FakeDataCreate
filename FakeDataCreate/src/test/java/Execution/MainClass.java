4be83421cb367df89c01b4feef1ff7b1c51a0b12
Kalpana-04 <58261460+Kalpana-04@users.noreply.github.com>
2020-10-20 13:56:10 +0530

    sfasf

--- FakeDataCreate/src/test/java/Execution/MainClass.java
+++ FakeDataCreate/src/test/java/Execution/MainClass.java
@@ -21,7 +21,7 @@ public class MainClass {
 		ref.createData(Constants.totalData);
 		/*datacreation d =  new datacreation();
 		d.createprotectionAllocatedValue();*/
-		
+		// Changes by Kalpana
 		
 		//Utils.generateFolderwithName();
 		
