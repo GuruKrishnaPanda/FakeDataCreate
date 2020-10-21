1ef33cbf791e6622026c93dd1a537f7f99aa8416
GuruKrishnaPanda <gurukrishnapanda@gmail.com>
2020-10-21 12:32:01 +0530

    Update MainClass.java

--- FakeDataCreate/src/test/java/Execution/MainClass.java
+++ FakeDataCreate/src/test/java/Execution/MainClass.java
@@ -16,12 +16,7 @@ public class MainClass {
 
 	public static void main(String[] args)  {
 		
-		Utils.archiveAvailableFiles();
-		ReusableFactory ref =  new ReusableFactory(); 
-
-		ref.createData(Constants.totalData);	
-
-		ref.createData(Constants.totalData);
+		
 		//new update
 
 
