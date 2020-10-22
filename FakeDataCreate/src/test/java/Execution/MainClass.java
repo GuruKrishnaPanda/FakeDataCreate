bc25d6ff5c9e234cb363e22a38496d4d1f4420c4
GuruKrishna <GuruKrishna@GuruKrishna>
2020-10-22 08:42:29 +0530

    commit

--- FakeDataCreate/src/test/java/Execution/MainClass.java
+++ FakeDataCreate/src/test/java/Execution/MainClass.java
@@ -2,10 +2,6 @@ package Execution;
 
 
 
-import java.io.IOException;
-import java.lang.reflect.Field;
-
-import pojoClases.Contract;
 import ReUsable.ReusableFactory;
 
 import Utilities.Constants;
@@ -16,13 +12,13 @@ public class MainClass {
 
 	public static void main(String[] args)  {
 		
-		Utils.archiveAvailableFiles();
-		ReusableFactory ref =  new ReusableFactory(); 
+		/*
+		 * Utils.archiveAvailableFiles(); ReusableFactory ref = new ReusableFactory();
+		 * 
+		 * ref.createData(Constants.totalData);
+		 */
 
-		ref.createData(Constants.totalData);	
-
-		ref.createData(Constants.totalData);
-		//new update
+		//new update from GuruSystem
 
 
 	}
