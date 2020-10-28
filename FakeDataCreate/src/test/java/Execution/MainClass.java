9d1d59d53e96a6c5e6456fb26121f86a07421fea
Asus <Asus@DESKTOP-0G2L8GS>
2020-10-28 06:19:18 -0700

    commit

--- FakeDataCreate/src/test/java/Execution/MainClass.java
+++ FakeDataCreate/src/test/java/Execution/MainClass.java
@@ -3,6 +3,7 @@ package Execution;
 import java.util.Hashtable;
 
 import ReUsable.ReusableFactory;
+import ReUsable.datacreation;
 import Utilities.DataUtil;
 import Utilities.GenericXLSXReader;
 import Utilities.Utils;
@@ -13,7 +14,7 @@ public class MainClass {
 	public static void main(String[] args)  
 	{
 
-		
+		datacreation d = new datacreation();
 	}
 
 }
