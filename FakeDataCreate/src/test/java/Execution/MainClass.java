048df84dc19fa31639570d1ea262a8e0f6156334
GuruKrishnaPanda <gurukrishnapanda@gmail.com>
2020-10-28 17:58:02 +0530

    Update MainClass.java

--- FakeDataCreate/src/test/java/Execution/MainClass.java
+++ FakeDataCreate/src/test/java/Execution/MainClass.java
@@ -14,28 +14,6 @@ public class MainClass {
 	{
 
 		
-		 //Utils.archiveAvailableFiles();
-		 //ReusableFactory ref = new ReusableFactory();
-		 
-		// ref.createData(Constants.totalData);
-
-	}
-		//ReusableFactory ref = new ReusableFactory();
-		//ref.createData(Constants.totalData);
-		//datacreation d=new datacreation();
-		//d.creditEquivalentOffBalanceSheetNominalAmount();
-	
-		
-		/*Utils.archiveAvailableFiles();
-		 GenericXLSXReader xlsx =  new GenericXLSXReader("C:\\Users\\aloor\\git\\FakeDataCreate\\FakeDataCreate\\resources\\configuration.xlsx");
-
-		DataUtil datautils =  new DataUtil();
-		Hashtable<String, String> data = datautils.getData("", xlsx);
-		System.out.println(data.size());
-		ReusableFactory ref = new ReusableFactory();
-		System.out.println(data.get("NoOfData"));
-		
-		 ref.createData( Integer.parseInt(data.get("NoOfData")));*/
-	
 	}
 
+}
